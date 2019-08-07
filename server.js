7b9bc3e5a605fc18cbe08c49ef808c99ab8fa050
Brandon Chupp <15858979+brandonchupp@users.noreply.github.com>
2019-08-07 11:29:31 -0400

    Add broadcast for sending to all client sessions

--- server.js
+++ server.js
@@ -80,13 +80,6 @@ let initSocket = (servo) => {
     var prompt_velocity = true;
 
     io.on('connection', function(socket) {
-        // Initialize readings to zero
-        socket.emit('drag', 0);
-        socket.emit('lift', 0);
-        socket.emit('velocity', 0);
-        socket.emit('static_pressure', 0);
-        socket.emit('total_pressure', 0);
-
 
         function velocity_set() {
             // Formula from:
@@ -95,14 +88,14 @@ let initSocket = (servo) => {
                 (dynamic_pressure * 27.6799)/
                 (1.325 * baro_pressure / (temp + 460))
             );
-            socket.emit('velocity', velocity);
+            socket.broadcast.emit('velocity', velocity);
             if (recording) {
                 recorded_data['velocity'].push(velocity);
             }
         }
 
         if (prompt_velocity) {
-            socket.emit('prompt_velocity');
+            socket.broadcast.emit('prompt_velocity');
         }
         socket.on('velocity_set', function(dict) {
             prompt_velocity = false;
@@ -114,7 +107,7 @@ let initSocket = (servo) => {
 
         // Continuously update the results of recording
         setInterval(function() {
-            socket.emit('update_record', recorded_data);
+            socket.broadcast.emit('update_record', recorded_data);
         }, 500);
 
         if (DEV_MODE) {
@@ -124,10 +117,10 @@ let initSocket = (servo) => {
                 static_pressure = Math.random();
                 total_pressure = Math.random();
                 dynamic_pressure = total_pressure - static_pressure;
-                socket.emit('lift', lift);
-                socket.emit('drag', drag);
-                socket.emit('static_pressure', static_pressure);
-                socket.emit('total_pressure', total_pressure);
+                socket.broadcast.emit('lift', lift);
+                socket.broadcast.emit('drag', drag);
+                socket.broadcast.emit('static_pressure', static_pressure);
+                socket.broadcast.emit('total_pressure', total_pressure);
                 if (!prompt_velocity) {
                     velocity_set();
                 }
@@ -137,7 +130,7 @@ let initSocket = (servo) => {
                     recorded_data['static_pressure'].push(static_pressure);
                     recorded_data['dynamic_pressure'].push(total_pressure - static_pressure);
                     recorded_data['total_pressure'].push(total_pressure);
-                    socket.emit('update_record', recorded_data);
+                    socket.broadcast.emit('update_record', recorded_data);
                 }
             }, 1000);
         } else {
@@ -159,7 +152,7 @@ let initSocket = (servo) => {
                     lift -= 0xFFFFFF;
                 }
                 // CALIBRATE READING HERE
-                socket.emit('drag', drag - drag_tare);
+                socket.broadcast.emit('drag', drag - drag_tare);
                 if (recording) {
                     recorded_data['drag'].push(drag - drag_tare);
                 }
@@ -171,11 +164,11 @@ let initSocket = (servo) => {
                     // is the voltage ratio, and the other numbers are in the given formula
                     static_pressure = Math.round((value*15.76/1024 + 1.54)*1000)/1000;
 
-                    socket.emit('static_pressure', static_pressure);
+                    socket.broadcast.emit('static_pressure', static_pressure);
 
                     if (recording) {
                         recorded_data['static_pressure'].push(static_pressure);
-                        socket.emit('update_record', recorded_data);
+                        socket.broadcast.emit('update_record', recorded_data);
                     }
                 });
 
@@ -184,10 +177,10 @@ let initSocket = (servo) => {
                     total_pressure = Math.round((value*15.76/1024 + 1.54)*1000)/1000;
                     dynamic_pressure = total_pressure - static_pressure;
 
-                    socket.emit('total_pressure', total_pressure);
+                    socket.broadcast.emit('total_pressure', total_pressure);
                     if (recording) {
                         recorded_data['total_pressure'].push(total_pressure);
-                        socket.emit('update_record', recorded_data);
+                        socket.broadcast.emit('update_record', recorded_data);
                     }
                 });
 
